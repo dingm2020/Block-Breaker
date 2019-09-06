8d18c0c8f6970b3477a5eba918f18cf7fa3c7e24
Maxwell Ding <mbding@gmail.com>
2019-09-06 01:57:53 -0500

    just finished first quiz (quiz #5)

---
+++ Block Breaker/Assets/Scripts/Ball.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    // Configuration parameters
+    [SerializeField] private Paddle paddle1;
+    [SerializeField] private float xPush = 2f;
+    [SerializeField] private float yPush = 10f;
+    
+    
+    // State
+    private Vector2 paddleToBallVector;
+    private bool hasStarted = false;
+
+    // Start is called before the first frame update
+    void Start() {
+        paddleToBallVector = transform.position - paddle1.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (!hasStarted) {
+            LockBallToPaddle();
+            BallLaunch();
+        }
+    }
+
+    private void LockBallToPaddle() {
+        Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
+        transform.position = paddleToBallVector + paddlePos;
+    }
+
+    private void BallLaunch() {
+        if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space)) {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(xPush, yPush);
+            hasStarted = true;
+        }
+    }
+}
---
+++ Block Breaker/Assets/Scripts/Paddle.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+    
+    // Configuration parameters
+    [SerializeField] private float screenWidth = 16f, minX = 1f, maxX = 15f;
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        float mousePos = Input.mousePosition.x / Screen.width * screenWidth;
+        Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y);
+        paddlePos.x = Mathf.Clamp(mousePos, minX, maxX);
+        transform.position = paddlePos;
+    }
+}
